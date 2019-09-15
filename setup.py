573e7f77f5986d3f71ba8fca46c37aeb947c211c
Dennis Wilson <dennis@d9w.xyz>
2019-09-15 22:23:19 +0200

    installation script, module organization

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages
+from distutils.core import setup
+
+setup(
+    name='pycgp',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'joblib',
+        'numpy'],
+    license='MIT',
+)
